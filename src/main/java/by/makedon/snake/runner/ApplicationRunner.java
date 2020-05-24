04e0360e82361688c60b6290410727ff46bd51d8
Yahor Makedon <emakedonw@gmail.com>
2020-05-24 14:27:47 +0300

    Commit.

--- src/main/java/by/makedon/snake/runner/ApplicationRunner.java
+++ src/main/java/by/makedon/snake/runner/ApplicationRunner.java
@@ -1,10 +1,24 @@
 package by.makedon.snake.runner;
 
+import by.makedon.snake.window.Window;
+import com.sun.org.slf4j.internal.Logger;
+import com.sun.org.slf4j.internal.LoggerFactory;
+
+import javax.swing.*;
+
 /**
  * @author Yahor Makedon
  */
 public class ApplicationRunner {
-    public static void main(String[] args) {
+    private static final Logger logger = LoggerFactory.getLogger(ApplicationRunner.class);
 
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            try {
+                new Window().showWindow();
+            } catch (Exception e) {
+                logger.error(e.getLocalizedMessage(), e);
+            }
+        });
     }
 }
--- src/main/java/by/makedon/snake/window/Window.java
+++ src/main/java/by/makedon/snake/window/Window.java
@@ -1,7 +1,31 @@
 package by.makedon.snake.window;
 
+import by.makedon.snake.util.Constants;
+import by.makedon.snake.util.ResourceUtil;
+
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * @author Yahor Makedon
  */
-public class Window {
+public class Window extends JFrame {
+    public Window() {
+        String frameName = ResourceUtil.getPropertyValue(Constants.FRAME_NAME);
+        int frameMinimumWidth = Integer.parseInt(ResourceUtil.getPropertyValue(Constants.FRAME_MINIMUM_WIDTH));
+        int frameMinimumHeight = Integer.parseInt(ResourceUtil.getPropertyValue(Constants.FRAME_MINIMUM_HEIGHT));
+
+        setTitle(frameName);
+        setMinimumSize(new Dimension(frameMinimumWidth, frameMinimumHeight));
+        setExtendedState(java.awt.Frame.MAXIMIZED_BOTH);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        setJMenuBar(new MenuBar());
+        add(new JScrollPane(DrawManager.getInstance().getDrawPanel()), BorderLayout.CENTER);
+    }
+
+    public void showWindow() {
+        setVisible(true);
+    }
 }
