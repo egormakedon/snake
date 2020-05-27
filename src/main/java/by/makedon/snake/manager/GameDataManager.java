7915ef721c4cb67354ad84290ce3e3027564635d
Yahor Makedon <emakedonw@gmail.com>
2020-05-27 21:03:04 +0300

    Add update pixel map.

--- src/main/java/by/makedon/snake/manager/GameDataManager.java
+++ src/main/java/by/makedon/snake/manager/GameDataManager.java
@@ -7,25 +7,28 @@ import by.makedon.snake.util.Constants;
 import by.makedon.snake.util.ResourceUtil;
 import by.makedon.snake.validator.Validator;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * @author Yahor Makedon
  */
 public final class GameDataManager {
-    private static final List<Pixel> snakeStartPositionPixelList;
+    public static final Integer KEY_FREE_PIXEL_LIST = 0;
+    public static final Integer KEY_SNAKE_PIXEL_LIST = 1;
+    public static final Integer KEY_APPLE_PIXEL_LIST = 2;
+
+    private static final List<Integer> snakeStartPositionCoordinateList;
     private static GameDataManager instance;
 
     static {
-        snakeStartPositionPixelList = loadSnakeStartPosition();
+        snakeStartPositionCoordinateList = loadSnakeStartPosition();
     }
 
-    private List<Pixel> mapPixelList;
+    private List<Pixel> gameMapPixelList;
     private Snake snake;
     private Apple apple;
+    private Map<Integer, List<Pixel>> updatePixelMap;
 
     private GameDataManager() {
     }
@@ -39,44 +42,45 @@ public final class GameDataManager {
     }
 
     public void createData(int width, int height) {
-        createMapPixelList(width, height);
+        createGameMapPixelList(width, height);
         createSnake(width, height);
         createApple();
+        createUpdatePixelMap();
+    }
+
+    public Optional<Pixel> getPixel(int x, int y) {
+        return gameMapPixelList.parallelStream()
+                .filter(pixel -> pixel.getX() == x && pixel.getY() == y)
+                .findAny();
     }
 
     public Optional<Pixel> getFreePixel() {
-        return mapPixelList.parallelStream()
+        return gameMapPixelList.parallelStream()
                 .filter(pixel -> !snake.getPixelList().contains(pixel))
                 .findAny();
     }
 
-    private static List<Pixel> loadSnakeStartPosition() {
+    private static List<Integer> loadSnakeStartPosition() {
         String snakeStartPosition = ResourceUtil.getPropertyValue(Constants.SNAKE_START_POSITION);
         String[] snakeStartPositionArray = snakeStartPosition.split(Constants.VERTICAL_SLASH);
 
         Validator.validateSnakeStartPosition(snakeStartPosition, snakeStartPositionArray);
 
-        List<Pixel> snakeStartPositionPixelList = new ArrayList<>(snakeStartPositionArray.length / 2);
-        for (int i = 0; i + 1 < snakeStartPositionArray.length; i+=2) {
-            int x = Integer.parseInt(snakeStartPositionArray[i]);
-            int y = Integer.parseInt(snakeStartPositionArray[i + 1]);
-
-            snakeStartPositionPixelList.add(new Pixel(x, y));
-        }
-
-        return snakeStartPositionPixelList;
+        return Arrays.stream(snakeStartPositionArray)
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
     }
 
-    private void createMapPixelList(int width, int height) {
-        mapPixelList = new ArrayList<>(width * height);
+    private void createGameMapPixelList(int width, int height) {
+        gameMapPixelList = new ArrayList<>(width * height);
 
         for (int i = 0; i < width; i++) {
             for (int j = 0; j < height; j++) {
-                mapPixelList.add(new Pixel(i, j));
+                gameMapPixelList.add(new Pixel(i, j));
             }
         }
 
-        Collections.shuffle(mapPixelList);
+        Collections.shuffle(gameMapPixelList);
     }
 
     private void createSnake(int width, int height) {
@@ -85,7 +89,17 @@ public final class GameDataManager {
         }
 
         List<Pixel> pixelList = new ArrayList<>(width * height);
-        pixelList.addAll(snakeStartPositionPixelList);
+        for (int i = 0; i + 1 < snakeStartPositionCoordinateList.size(); i+=2) {
+            int x = snakeStartPositionCoordinateList.get(i);
+            int y = snakeStartPositionCoordinateList.get(i + 1);
+
+            Optional<Pixel> optionalPixel = getPixel(x, y);
+            if (optionalPixel.isPresent()) {
+                pixelList.add(optionalPixel.get());
+            } else {
+                throw new IllegalArgumentException("Snake start position doesn't match to the window size");
+            }
+        }
 
         snake.setPixelList(pixelList);
     }
@@ -95,6 +109,27 @@ public final class GameDataManager {
             apple = new Apple();
         }
 
-        apple.setPixel(getFreePixel().get());
+        Optional<Pixel> optionalPixel = getFreePixel();
+        if (optionalPixel.isPresent()) {
+            apple.setPixel(optionalPixel.get());
+        } else {
+            throw new IllegalArgumentException("Snake start position occupy full window size");
+        }
+    }
+
+    private void createUpdatePixelMap() {
+        if (updatePixelMap == null) {
+            updatePixelMap = new HashMap<>(3);
+        }
+
+        List<Pixel> freePixelList = gameMapPixelList.parallelStream()
+                .filter(pixel -> !snake.getPixelList().contains(pixel) && !apple.getPixel().equals(pixel))
+                .collect(Collectors.toList());
+        List<Pixel> snakePixelList = snake.getPixelList();
+        List<Pixel> applePixelList = Collections.singletonList(apple.getPixel());
+
+        updatePixelMap.put(KEY_FREE_PIXEL_LIST, freePixelList);
+        updatePixelMap.put(KEY_SNAKE_PIXEL_LIST, snakePixelList);
+        updatePixelMap.put(KEY_APPLE_PIXEL_LIST, applePixelList);
     }
 }
